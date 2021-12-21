2165744a1a0b860b2a576a3ba4a5613c89c66f5b
EdwinTsai <edwin901231@gmail.com>
2021-12-21 10:56:40 +0800

    update

--- FinalProject/little_dinosaur.v
+++ FinalProject/little_dinosaur.v
@@ -61,8 +61,20 @@ module LD_state(spd_ldi , up , down , map_ld , state) ; // output a state
 
 endmodule
 
-module Obstacle(output_obstacle) ; // create a new obstacle
-    output [1:0] output_obstacle ;
+module Obstacle (unit_clk , gap , spawn_obstacle_7 , spawn_obstacle_6 , spawn_obstacle_5 , spawn_obstacle_4 , spawn_obstacle_3
+    ,spawn_obstacle_2 , spawn_obstacle_1 , spawn_obstacle_0 ) ;
+    input unit_clk ;
+    input [1:0] gap ;
+    output[1:0] spawn_obstacle_7 ;
+    output[1:0] spawn_obstacle_6 ;
+    output[1:0] spawn_obstacle_5 ;
+    output[1:0] spawn_obstacle_4 ;
+    output[1:0] spawn_obstacle_3 ;
+    output[1:0] spawn_obstacle_2 ;
+    output[1:0] spawn_obstacle_1 ;
+    output[1:0] spawn_obstacle_0 ;
+
+    
 
 endmodule
 
@@ -111,11 +123,90 @@ module Score(unit_clk,restart,score_out1,score_out2,score_out3,score_out4);//The
 		end
 endmodule
 
-module Refresh(unit_clk , col1 , col2 , restart , dot_row1 , dot_row2 , dot_col1 , dot_col2) ;
-    input unit_clk , restart , stop , start ;
-    input [7:0] col1 , col2 ;
-    output reg [7:0] dot_row1 , dot_row2 , dot_col1 , dot_col2 ;
-    reg[2:0] row_count ; 
+/* top module */
+module little_dinosaur(clock , restart , stop , up , down , ssd_out1 , ssd_out2 , ssd_out3 , ssd_out4 , dot_row1 , dot_col1 , dot_row2, dot_col2 , life ) ;
+    
+    /*device*/
+    input clock ; 
+    input restart , start , stop , up , down ; // button
+    output[6:0] ssd_out1 , ssd_out2 , ssd_out3 , ssd_out4 ; // Seven Segments Display
+    output reg [7:0] dot_row1 , dot_col1 , dot_row2 , dot_col2 ; // show the picture in the dot matrix
+    wire [3:0] score_out1,score_out2,score_out3,score_out4; // connect to the ssd 
+    wire unit_clk ; // unit_clk represents the time to refresh the dot matrix   
+    
+    /*map*/
+    wire[7:0] col1[7:0] ,  col2[7:0]  ; // Combine the mv_map and map_ld together , and send it to dot_col 
+    reg[7:0] mv_map[7:0][1:0] ; // the map only need to record the column  
+    wire [3:0] map_ld[7:0] ; // the map of the little dinosaur
+    reg[15:0] record_obstacle[7:0] ; // position of obstacle 
+    wire spd_map ;
+    wire [7:0] temp[7:0] ;
+   
+    /*obstacle*/ 
+    wire[1:0] spawn_obstacle[7:0] ; // generate obstacles
+    reg[1:0] obstacle[7:0] ; // spawn_obstacle give the generated one to obstacle
+    reg[1:0] gap ; // used to control the obstacle not too close to other obstacles
+
+    /*other*/
+    output reg [2:0] life ; // the life of the dinosaur 
+    wire[3:0] state ; // the state number of the little dinosaur 
+    wire hit ; // check if the little dinosaur was hit or not 
+    wire score ;
+    reg [2:0] row_count ;
+
+    ssd s1(.in(score_out1),.out(ssd_out1));
+    ssd s2(.in(score_out2),.out(ssd_out2));
+    ssd s3(.in(score_out3),.out(ssd_out3));
+    ssd s4(.in(score_out4),.out(ssd_out4));
+
+
+    Unit_fd f2 (.clk_in(clock) , .reset(restart) , .clk_out(unit_clk)) ; // frequency divider 
+    // LD_state m1 (.state(state)) ;
+
+    // create a new obstacle
+    Obstacle m2_0 (unit_clk , gap , spawn_obstacle[7] , spawn_obstacle[6] , spawn_obstacle[5] , spawn_obstacle[4] , spawn_obstacle[3]
+    ,spawn_obstacle[2] , spawn_obstacle[1] , spawn_obstacle[0] ) ;
+
+    // check whether it was hit or not 
+    Hit m3 (.hit(hit)) ;
+    // the score is depends on game speed 
+    Score m4 (unit_clk,restart,score_out1,score_out2,score_out3,score_out4);
+    // refresh the map
+    
+    // Combine the map
+    
+    assign temp[0] = mv_map[0][0] ;
+    assign col1[0] = {(temp[0][7:4] | map_ld[0]) , temp[0][3:0]}  ; 
+    assign col2[0] = mv_map[0][1] ;
+
+    assign temp[1] = mv_map[1][0] ;
+    assign col1[1] = {(temp[1][7:4] | map_ld[1]) , temp[1][3:0]}  ; 
+    assign col2[1] = mv_map[1][1] ;
+
+    assign temp[2] = mv_map[2][0] ;
+    assign col1[2] = {(temp[2][7:4] | map_ld[2]) , temp[2][3:0]}  ; 
+    assign col2[2] = mv_map[2][1] ;
+
+    assign temp[3] = mv_map[3][0] ;
+    assign col1[3] = {(temp[3][7:4] | map_ld[3]) , temp[3][3:0]}  ; 
+    assign col2[3] = mv_map[3][1] ;
+
+    assign temp[4] = mv_map[4][0] ;
+    assign col1[4] = {(temp[4][7:4] | map_ld[4]) , temp[4][3:0]}  ; 
+    assign col2[4] = mv_map[4][1] ;
+
+    assign temp[5] = mv_map[5][0] ;
+    assign col1[5] = {(temp[5][7:4] | map_ld[5]) , temp[5][3:0]}  ; 
+    assign col2[5] = mv_map[5][1] ;
+
+    assign temp[6] = mv_map[6][0] ;
+    assign col1[6] = {(temp[6][7:4] | map_ld[6]) , temp[6][3:0]}  ; 
+    assign col2[6] = mv_map[6][1] ;
+
+    assign temp[7] = mv_map[7][0] ;
+    assign col1[7] = {(temp[7][7:4] | map_ld[7]) , temp[7][3:0]}  ; 
+    assign col2[7] = mv_map[7][1] ;
+
 
     always@(posedge unit_clk , negedge restart )
     begin
@@ -129,146 +220,92 @@ module Refresh(unit_clk , col1 , col2 , restart , dot_row1 , dot_row2 , dot_col1
                     case(row_count)
                     3'd 0 :
                     begin
-                        dot_row1 <= 8'b 01111111 ; 
-                        dot_row2 <= 8'b 01111111 ; 
+                        dot_row1 <= 8'b 01111111 ;
+                        dot_row2 <= 8'b 01111111 ;
                     end
                     3'd 1 :
                     begin
-                        dot_row1 <= 8'b 10111111 ; 
-                        dot_row2 <= 8'b 10111111 ; 
+                        dot_row1 <= 8'b 10111111 ;
+                        dot_row2 <= 8'b 10111111 ;
                     end
                     3'd 2 :
                     begin
-                        dot_row1 <= 8'b 11011111 ; 
-                        dot_row2 <= 8'b 11011111 ; 
+                        dot_row1 <= 8'b 11011111 ;
+                        dot_row2 <= 8'b 11011111 ;
                     end
                     3'd 3 :
                     begin
-                        dot_row1 <= 8'b 11101111 ; 
-                        dot_row2 <= 8'b 11101111 ; 
+                        dot_row1 <= 8'b 11101111 ;
+                        dot_row2 <= 8'b 11101111 ;
                     end
                     3'd 4 :
                     begin
-                        dot_row1 <= 8'b 11110111 ; 
-                        dot_row2 <= 8'b 11110111 ; 
+                        dot_row1 <= 8'b 11110111 ;
+                        dot_row2 <= 8'b 11110111 ;
                     end
                     3'd 5 :
                     begin
-                        dot_row1 <= 8'b 11111011 ; 
-                        dot_row2 <= 8'b 11111011 ; 
+                        dot_row1 <= 8'b 11111011 ;
+                        dot_row2 <= 8'b 11111011 ;
                     end
                     3'd 6 :
                     begin
-                        dot_row1 <= 8'b 11111101 ; 
-                        dot_row2 <= 8'b 11111101 ; 
+                        dot_row1 <= 8'b 11111101 ;
+                        dot_row2 <= 8'b 11111101 ;
                     end
                     3'd 7 :
                     begin
-                        dot_row1 <= 8'b 11111110 ; 
-                        dot_row2 <= 8'b 11111110 ; 
+                        dot_row1 <= 8'b 11111110 ;
+                        dot_row2 <= 8'b 11111110 ;
                     end
                 endcase
 
                 case(row_count)
                     3'd 0 :
                     begin
-                        dot_col1 <= col1 ; 
-                        dot_col2 <= col2 ; 
+                        dot_col1 <= col1[0] ;
+                        dot_col2 <= col2[0] ;
                     end
                     3'd 1 :
                     begin
-                        dot_col1 <= col1 ; 
-                        dot_col2 <= col2 ; 
+                        dot_col1 <= col1[1] ;
+                        dot_col2 <= col2[1] ;
                     end
                     3'd 2 :
                     begin
-                        dot_col1 <= col1 ; 
-                        dot_col2 <= col2 ; 
+                        dot_col1 <= col1[2] ;
+                        dot_col2 <= col2[2] ;
                     end
                     3'd 3 :
                     begin
-                        dot_col1 <= col1 ; 
-                        dot_col2 <= col2 ; 
+                        dot_col1 <= col1[3] ;
+                        dot_col2 <= col2[3] ;
                     end
                     3'd 4 :
                     begin
-                        dot_col1 <= col1 ; 
-                        dot_col2 <= col2 ; 
-                    end 
+                        dot_col1 <= col1[4] ;
+                        dot_col2 <= col2[4] ;
+                    end
                     3'd 5 :
                     begin
-                        dot_col1 <= col1 ; 
-                        dot_col2 <= col2 ; 
+                        dot_col1 <= col1[5] ;
+                        dot_col2 <= col2[5] ;
                     end
                     3'd 6 :
                     begin
-                        dot_col1 <= col1 ; 
-                        dot_col2 <= col2 ; 
+                        dot_col1 <= col1[6] ;
+                        dot_col2 <= col2[6] ;
                     end
                     3'd 7 :
                     begin
-                        dot_col1 <= col1 ; 
-                        dot_col2 <= col2 ; 
+                        dot_col1 <= col1[7] ;
+                        dot_col2 <= col2[7] ;
                     end
                     endcase
                 end
-    end
-endmodule 
-
-
-/* top module */
-module little_dinosaur(clock , restart , stop , up , down , ssd_out1 , ssd_out2 , ssd_out3 , ssd_out4 , dot_row1 , dot_col1 , dot_row2, dot_col2 , life ) ;
+        end
     
-    /*device*/
-    input clock ; 
-    input restart , start , stop , up , down ; // button
-    output[6:0] ssd_out1 , ssd_out2 , ssd_out3 , ssd_out4 ; // Seven Segments Display
-    output[7:0] dot_row1 , dot_col1 , dot_row2 , dot_col2 ; // show the picture in the dot matrix
-    wire [3:0] score_out1,score_out2,score_out3,score_out4; // connect to the ssd 
-    wire unit_clk ; // unit_clk represents the time to refresh the dot matrix   
-    
-    /*map*/
-    wire [7:0]  col1 ,  col2  ; // Combine the mv_map and map_ld together , and send it to dot_col 
-    reg[7:0] mv_map[1:0] ; // the map 
-    wire [3:0] map_ld ; // the map of the little dinosaur
-    reg[15:0] record_obstacle ; // position of obstacle 
-    wire spd_map ;
-    wire [7:0] temp ;
-   
-    /*obstacle*/ 
-    wire[1:0] spawn_obstacle ; // generate obstacles
-    reg[1:0] obstacle; // spawn_obstacle give the generated one to obstacle
-    reg[1:0] gap ; // used to control the obstacle not too close to other obstacles
-
-    /*other*/
-    output reg [2:0] life ; // the life of the dinosaur 
-    wire[3:0] state ; // the state number of the little dinosaur 
-    wire hit ; // check if the little dinosaur was hit or not 
-    wire score ;
-
-    ssd s1(.in(score_out1),.out(ssd_out1));
-    ssd s2(.in(score_out2),.out(ssd_out2));
-    ssd s3(.in(score_out3),.out(ssd_out3));
-    ssd s4(.in(score_out4),.out(ssd_out4));
-
-
-    Unit_fd f2 (.clk_in(clock) , .reset(restart) , .clk_out(unit_clk)) ; // frequency divider 
-    // LD_state m1 (.state(state)) ;
-
-    // create a new obstacle
-    Obstacle m2 (.output_obstacle(spawn_obstacle)) ;
-    // check whether it was hit or not 
-    Hit m3 (.hit(hit)) ;
-    // the score is depends on game speed 
-    Score m4 (unit_clk,restart,score_out1,score_out2,score_out3,score_out4);
-    // refresh the map
-    Refresh m5 (unit_clk , col1 , col2 , restart , dot_row1 , dot_row2 , dot_col1 , dot_col2) ;
-    
-    // Combine the map
-    assign temp = mv_map[0] ;
-    assign col1 = {(temp[7:4] | map_ld) , temp[3:0]}  ; 
-    assign col2 = mv_map[1] ;
-
+    integer i ;
     // control the moving map 
     always@(posedge unit_clk , negedge stop)
     begin
@@ -277,17 +314,29 @@ module little_dinosaur(clock , restart , stop , up , down , ssd_out1 , ssd_out2 
                 if(gap == 2'd 3)
                     begin
                     gap <= 2'd 0 ;
-                    obstacle <= spawn_obstacle ;
+                    for(i=0 ; i< 8  ; i++)
+                        begin
+                            obstacle[i] <= spawn_obstacle[i] ;
+                        end
                     end
                 else
                     begin
-                    obstacle <= obstacle ;
-                    gap <= gap+1 ;
+                    for(i = 0 ; i < 8 ; i++)
+                        begin
+                            obstacle[i] <= obstacle[i] ;
+                            gap <= gap+1 ;
+                        end
                     end
-                {mv_map[0],mv_map[1],obstacle} <= {mv_map[0],mv_map[1],obstacle} << 1 ;
+
+                for(i = 0 ; i < 8 ; i++)
+                    begin
+                        {mv_map[i][0],mv_map[i][1],obstacle[i]} <= {mv_map[i][0],mv_map[i][1],obstacle[i]} << 1 ;
+                    end
             end
         else
-            {mv_map[0],mv_map[1],obstacle} <= {mv_map[0],mv_map[1],obstacle} ;
+            for(i = 0 ; i < 8 ; i++)
+                begin
+                    {mv_map[i][0],mv_map[i][1],obstacle[i]} <= {mv_map[i][0],mv_map[i][1],obstacle[i]} ;
+                end
     end
-
 endmodule
